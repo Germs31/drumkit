ac3263b1e3610fcc30f877bc22ce536a588efd3f
German Nunez <german90nunez@gmail.com>
2020-01-15 21:26:45 -0800

    dom events created, basic css created

---
+++ app.js
@@ -0,0 +1,20 @@
+const kick = document.querySelector('#kick');
+const snare = document.querySelector('#snare');
+const hats = document.querySelector('#hats');
+const crash = document.querySelector('#crash');
+
+kick.addEventListener('click', () => {
+    console.log('kick kick');
+})
+
+snare.addEventListener('click', () => {
+    console.log('snare snare');
+})
+
+hats.addEventListener('click', () => {
+    console.log('hat hat');
+})
+
+crash.addEventListener('click', () => {
+    console.log('crash crash');
+})
